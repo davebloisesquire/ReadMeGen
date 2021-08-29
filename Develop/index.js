19cd661e455538e47c429fdabf2b8ba33b919c7d
theDaveB <davebloisesquire@gmail.com>
2021-08-29 11:17:38 -0700

    Adding License info

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const genMark = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'projName',
+    message: 'Project Name?',
+  },
+  {
+    type: 'input',
+    name: 'projDesc',
+    message: 'Description?',
+  },
+  {
+    type: 'input',
+    name: 'projInst',
+    message: 'Installation Instructions?',
+  },
+  {
+    type: 'input',
+    name: 'projUsag',
+    message: 'Usage Information?',
+  },
+  {
+    type: 'input',
+    name: 'projGuides',
+    message: 'Project Contribution Guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'projTest',
+    message: 'Project Testing Instructions?',
+  },
+  {
+    type: 'list',
+    name: 'projLicense',
+    choices: ['None', 'GNU'],
+    message: 'Choose your license?',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    "None": ""
+    "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "Boost SL 1.0": "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    "BSD 3": "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    "CC0": "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)",
+    "Eclipse": "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+    "GNU": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  }
+  return badges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
-
+const licenseLink = {
+  "None": ""
+  "Apache 2.0": "(https://opensource.org/licenses/Apache-2.0)",
+  "Boost SL 1.0": "(https://www.boost.org/LICENSE_1_0.txt)",
+  "BSD 3": "(https://opensource.org/licenses/BSD-3-Clause)",
+  "CC0": "(http://creativecommons.org/publicdomain/zero/1.0/)",
+  "Eclipse": "(https://opensource.org/licenses/EPL-1.0)",
+  "GNU": "(https://www.gnu.org/licenses/gpl-3.0)",
+  "MIT": "(https://opensource.org/licenses/MIT)",
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
